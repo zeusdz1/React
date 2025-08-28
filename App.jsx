34cc57e7ccb5da8aa4bb5d63f1a5071a668e65d5
zeusdz1 <127206164+zeusdz1@users.noreply.github.com>
2025-08-28 13:26:43 +0100

    Commit 24

--- App.jsx
+++ App.jsx
@@ -1,33 +1,15 @@
 import React from 'react';
-import Card from './components/Card';
-
-const App = () => {
-  const cardData = [
-    {
-      title: "React Development",
-      description: "Learn how to build web applications with React and Tailwind CSS.",
-      buttonText: "Learn More",
-      imageUrl: "https://picsum.photos/400/200?random=1"
-    },
-    {
-      title: "Tailwind CSS Mastery",
-      description: "Master the art of rapid UI development with Tailwind CSS.",
-      buttonText: "Explore",
-      imageUrl: "https://picsum.photos/400/200?random=2"
-    }
-  ];
+import Header from './components/Header';
 
 
+function App() {
+  
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold text-center mb-8">My Card Application</h1>
-      <div className="flex flex-wrap justify-center">
-        {cardData.map((card, index) => (
-          <Card key={index} {...card} />
-        ))}
-      </div>
-      </div>
+    <div>
+      <Header />
+      {/* additional components goes here */}
+    </div>
   );
-};
+}
 
 export default App
